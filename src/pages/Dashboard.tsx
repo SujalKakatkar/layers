bd3e53b398871d9036da392382ed30afc5949d02
SujalKakatkar <sujalkakatkar54@gmail.com>
2026-03-30 08:09:06 +0530

    feat: initial commit

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -40,46 +40,48 @@ function Dashboard () {
   }
 
   return (
-    <div className="p-8">
-      {/* user details */}
-      <section className="h-15 px-4 rounded-xl flex items-center my-2">
-        <div className="flex-1">
-        <h1 className="">username</h1>
+    <div className=" flex justify-center ">
+      <div className="px-2 sm:min-w-2xl xl:min-w-6xl ">
+        {/* user details */}
+        <section className="h-15 px-4 rounded-xl flex items-center my-2">
+          <div className="flex-1">
+            <h1 className="">username</h1>
 
-        </div>
-        <div>
-          <Button size={"icon"} variant={"bordered"}>
-            <LogOut/>
-          </Button>
-        </div>
-      </section>
+          </div>
+          <div>
+            <Button size={"icon"} variant={"bordered"}>
+              <LogOut />
+            </Button>
+          </div>
+        </section>
 
-      <Separator/>
+        <Separator />
 
-      <section className="py-3">
-        <button
-          onClick={createCanvas}
-          className="bg-theme-accent h-[10rem] w-[15rem] text-white flex items-center justify-center px-4 py-2 rounded"
-        >
-          <Plus />
-          Create Canvas
-        </button>
+        <section className="py-3">
+          <button
+            onClick={createCanvas}
+            className="bg-theme-accent h-[10rem] w-[15rem] text-white flex items-center justify-center px-4 py-2 rounded"
+          >
+            <Plus />
+            Create Canvas
+          </button>
 
-        <div className="mt-6 space-y-2">
+          <div className="mt-6 space-y-2">
 
-          {canvases.map(canvas => (
-            <div
-              key={canvas.id}
-              onClick={() => navigate(`/canvas/${canvas.id}`)}
-              className="p-4 border rounded cursor-pointer hover:bg-gray-100"
-            >
-              {canvas.title}
-            </div>
-          ))}
+            {canvases.map(canvas => (
+              <div
+                key={canvas.id}
+                onClick={() => navigate(`/canvas/${canvas.id}`)}
+                className="p-4 border rounded cursor-pointer hover:bg-gray-100"
+              >
+                {canvas.title}
+              </div>
+            ))}
 
-        </div>
+          </div>
 
-      </section>
+        </section>
+      </div>
     </div>
   )
 }
